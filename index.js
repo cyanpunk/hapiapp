05173fdb6a51cdc491436299e6b7e23f0f23d7a7
Austris Landmanis <austris@chevron.lv>
2015-07-09 13:58:40 +0300

    Migrated to jade template

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 
 var Colors = require('colors');
 var Colorterm = require('colorterm');
+var Good = require('good');
+var GoodConsole = require('good-console');
 var Hapi = require('hapi');
-var Slm = require('slm');
-var SlmMarkdown = require('slm-markdown');
+var Jade = require('jade');
+// var Slm = require('slm');
+// var SlmMarkdown = require('slm-markdown');
 
-SlmMarkdown.register(Slm.template);
+// SlmMarkdown.register(Slm.template);
 
 var console = new Colorterm();
 var server = new Hapi.Server();
@@ -18,7 +21,7 @@ server.connection({
 
 server.views({
   engines: {
-    'slm': Slm
+    jade: Jade
   },
   // Commented out, cause some Error
   // TODO: Create new issue on GitHub
@@ -41,13 +44,60 @@ server.route({
   }
 });
 
-server.start(function () {
-  console.log('Server successfully started...');
-  console.info('Server successfully started...');
-  console.warn('Server successfully started...');
-  console.error('Server successfully started...');
-  // console.dir( {bar: "This is a console.dir message", test: "yo", username: 'rafeca', url: 'https://github.com/rafeca', twitter_account: 'https://twitter.com/rafeca', projects: ['prettyprint', 'connfu'] } );
-  console.dir(server.info);
-  console.log('oh yeah!')
-  // server.log('Server started at ' + server.info.uri)
+var options = {
+  opsInterval: 15000,
+  reporters: [
+    {
+      reporter: GoodConsole,
+      events: {
+        ops: '*',
+        error: '*',
+        log: '*',
+        response: '*',
+        request: '*'
+      }
+    }
+  ]
+  // }, {
+  //   reporter: require('good-file'),
+  //   events: { ops: '*' },
+  //   config: './test/fixtures/awesome_log'
+  // }, {
+  //   reporter: 'good-http',
+  //   events: { error: '*' },
+  //   config: {
+  //     endpoint: 'http://prod.logs:3000',
+  //     wreck: {
+  //       headers: { 'x-api-key' : 12345 }
+  //     }
+  //   }
+  // }]
+};
+
+server.register({
+  register: Good,
+  options: options
+}, function (err) {
+
+  if (err) {
+    console.error(err);
+  }
+  else {
+    server.start(function () {
+      console.info('Server started successfully.');
+      console.dir(server.info);
+      server.log({data: "fsdfs"});
+    });
+  }
 });
+
+// server.start(function () {
+//   console.log('Server successfully started...');
+//   console.info('Server successfully started...');
+//   console.warn('Server successfully started...');
+//   console.error('Server successfully started...');
+//   // console.dir( {bar: "This is a console.dir message", test: "yo", username: 'rafeca', url: 'https://github.com/rafeca', twitter_account: 'https://twitter.com/rafeca', projects: ['prettyprint', 'connfu'] } );
+//   console.dir(server.info);
+//   console.log('oh yeah!')
+//   // server.log('Server started at ' + server.info.uri)
+// });
